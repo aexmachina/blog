c19ef16515a8d39665a55a5167baf7b3c7685e29
Simon Wade <simon.wade@gmail.com>
2015-01-23 21:34:27 +1100

    Fixes

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-(cd ember-demos && npm install && bower install) \
+(cd modules/basement && npm install) \
+  && (cd ember-demos && npm install && bower install) \
   && (cd content/themes/aexmachina \
     && npm install && bower install)
